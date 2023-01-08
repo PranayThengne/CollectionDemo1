e228f7e1738416a093336239b89a0737d0e08e8e
PranayThengne <pranaythengne96@gmail.com>
2023-01-08 18:33:15 +0530

    Added linkedList demo code

---
+++ src/com/git/arraylist/LinkedListDemo.java
@@ -0,0 +1,15 @@
+package com.git.arraylist;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListDemo {
+
+	public static void main(String[] args) {
+		List<Integer> list=new LinkedList<Integer>();
+		list.add(10);
+		list.add(20);
+		list.add(33);
+		System.out.println(list);
+	}
+}
